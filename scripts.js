600744871c0b73aa884f79d0755c097fad34bfba
ujvalmech <ujval.mech@gmail.com>
2024-08-30 20:27:31 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Handle the navbar toggle
-    const navbarToggle = document.querySelector('.navbar-toggle');
+    const menuToggle = document.querySelector('.navbar-toggle');
     const navbarMenu = document.querySelector('.navbar-menu');
 
-    navbarToggle.addEventListener('click', function() {
+    menuToggle.addEventListener('click', function() {
         navbarMenu.classList.toggle('active');
     });
 
-    // Smooth scrolling to top
-    const backToHomeButton = document.querySelector('.back-to-home');
-
-    backToHomeButton.addEventListener('click', function() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 768) {
+            navbarMenu.classList.remove('active');
+        }
     });
 });
