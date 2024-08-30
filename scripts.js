23762145e1a0719765ef007e1efd4142c013c9c6
ujvalmech <ujval.mech@gmail.com>
2024-08-30 17:58:38 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const backToHomeButton = document.querySelector('.back-to-home');
+    const navbarMenu = document.querySelector('.navbar-menu');
 
+    // Show/Hide "Back to Home" button based on scroll position
     window.addEventListener('scroll', function() {
         if (window.scrollY > 300) {
             backToHomeButton.style.display = 'block';
         } else {
             backToHomeButton.style.display = 'none';
         }
+    });
+
+    // Toggle Navbar menu on smaller screens
+    document.querySelector('.navbar-logo').addEventListener('click', function() {
+        navbarMenu.classList.toggle('active');
     });
 });
